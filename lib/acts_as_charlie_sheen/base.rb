cf792809f6777daeaa8b79b2fbdc6e9978cff858
alicetragedy <girl@alicetragedy.org>
2014-02-11 18:20:27 +0100

    add more charlie sheen

--- lib/acts_as_charlie_sheen/base.rb
+++ lib/acts_as_charlie_sheen/base.rb
@@ -12,6 +12,9 @@ module ActsAsCharlieSheen
           "I am on a drug. It's called Charlie Sheen.", 
           "I have defeated this earthworm with my words. Imagine what I would have done with my fire breathing fists.",
           "You can't process me with a normal brain.",
+          "I have one speed. I have one gear: Go.",
+          "I’m shakin’ a tree.  I’m shakin’ all the trees.",
+          "Dying is for fools, amateurs.",
           "DUH!!!"].sample
       end
     end
